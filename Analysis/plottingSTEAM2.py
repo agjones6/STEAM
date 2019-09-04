57385df14375e29cc7918669b551dcf8ee7000e7
Andy <agjones6@ncsu.edu>
2019-09-04 16:08:56 -0400

    Python data processing script
    - Started a python script that will read the output from the code

---
+++ Analysis/plottingSTEAM2.py
@@ -0,0 +1,96 @@
+# %% File to read in the output from the STEAM code
+import numpy as np
+import matplotlib as mpl
+import os
+from dataclasses import dataclass
+# import os.path
+from os import path
+import csv
+import pandas as pd
+
+# Allocating an object to store all of the data
+@dataclass
+class allData:
+    type: any
+    name: any
+    data: any
+
+allData.type = []
+allData.name = []
+allData.data = []
+
+# Reading through every file in the output file director
+outputFileLocation = "OutputFiles"
+for filename in os.listdir(outputFileLocation):
+    # Trying to open the files
+    cFileString = outputFileLocation + "/" + filename
+    if not path.exists(cFileString):
+        print("file: ", filename, "didn't open")
+        break
+    elif not (".csv" in cFileString):
+        print("Not a csv")
+        break
+
+    # Opening the file
+    count_line = 1
+    data = csv.reader(open(cFileString))
+    # print(data)
+    tst = []
+    # next(data)
+    # for row in data:
+    #     tst.append(row)
+    #
+    #     count_line = count_line + 1
+    #     if (count_line > 10):
+    #         break
+    #
+    # print(tst)
+    # exit()
+
+    # # Reading the first line of the file to determine if it is the right type
+    # count_line = 1
+    for spltLine in data:
+
+        # This checks to see if the file is a csv that is expected. (Skips files not formatted correctly)
+        # try:
+        #     dumCheck = cLine.split(",")[1]
+        # except:
+        #
+        #     break
+
+        # Pulling the entire line split into sections
+        # spltLine  = cLine.split(",")
+
+        # Pulling the first word of the csv to decide how to handle the file
+        if (count_line == 1):
+            if (spltLine[0] == "MATRIX"):
+                allData.name.append(spltLine[1])
+                allData.type.append("matrix")
+            else:
+                for i in range(len(spltLine)):
+                    if (spltLine[i] != ""):
+                        allData.name.append(spltLine[i])
+                        allData.type.append("value")
+        else:
+            cData = []
+
+            for i in range(len(spltLine)):
+                # print(np.array(float(spltLine[i].strip())))
+                # print(spltLine[i])
+                if (spltLine[i] != ""):
+                    cData.append(np.array(float(spltLine[i].strip())))
+            if (allData.type[-1] == "matrix"):
+                if (count_line == 2):
+                    allData.data.append(np.array(cData))
+                else:
+                    allData.data[-1] = np.vstack((allData.data[-1],cData))
+
+        count_line = count_line + 1
+        if (count_line > 1e6):
+            break
+
+    # break
+
+
+# print(allData.data[0][:][:].shape)
+print(allData.data[0][:][:].shape)
