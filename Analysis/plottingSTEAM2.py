73afbcb41666096ea17bd2fdb25d9b341b3712fd
Andy <agjones6@ncsu.edu>
2019-09-05 16:13:08 -0400

    Basic python plotting
    - The ability to plot with python was acheived. This is very basic and rudimentary. One string will yeild one plot

--- Analysis/plottingSTEAM2.py
+++ Analysis/plottingSTEAM2.py
@@ -1,13 +1,25 @@
 # %% File to read in the output from the STEAM code
 import numpy as np
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import os
-import dataclasses 
+import dataclasses
 from dataclasses import dataclass
 # import os.path
 from os import path
 import csv
 import pandas as pd
+
+# Functions that are to be used
+def find_index(name_matrix,desired_string):
+    # This function is made to search the matrix of strings and find the index of it
+    for i in range(len(name_matrix)):
+        if name_matrix[i] == desired_string:
+            break
+
+    return i
+
+def find_data(data_matrix,name_matrix,desired_string):
+    return data_matrix[find_index(name_matrix, desired_string)]
 
 # Allocating an object to store all of the data
 @dataclass
@@ -16,11 +28,16 @@ class allData:
     name: any
     data: any
     File: any
-
 allData.type = []
 allData.name = []
 allData.data = []
-allData.File = []
+allData.file = []
+
+# User Input
+myStr = "rho_CR"
+
+# This is the number of lines to skip when pulling in data
+skip_lines_num = 30
 
 # Reading through every file in the output file director
 outputFileLocation = "OutputFiles"
@@ -36,33 +53,13 @@ for filename in os.listdir(outputFileLocation):
 
     # Opening the file
     count_line = 1
-    allData.File.append(csv.reader(open(cFileString)))
-    # print(data)
-    tst = []
-    # next(data)
-    # for row in data:
-    #     tst.append(row)
-    #
-    #     count_line = count_line + 1
-    #     if (count_line > 10):
-    #         break
-    #
-    # print(tst)
-    # exit()
+    allData.file.append(csv.reader(open(cFileString)))
 
-    # # Reading the first line of the file to determine if it is the right type
-    # count_line = 1
-    for spltLine in allData.File[-1]:
+    # This variable is used to pull only as much data as is desired
+    sk_count = skip_lines_num
+    for spltLine in allData.file[-1]:
 
-        # This checks to see if the file is a csv that is expected. (Skips files not formatted correctly)
-        # try:
-        #     dumCheck = cLine.split(",")[1]
-        # except:
-        #
-        #     break
 
-        # Pulling the entire line split into sections
-        # spltLine  = cLine.split(",")
 
         # Pulling the first word of the csv to decide how to handle the file
         if (count_line == 1):
@@ -74,26 +71,60 @@ for filename in os.listdir(outputFileLocation):
                     if (spltLine[i] != ""):
                         allData.name.append(spltLine[i])
                         allData.type.append("value")
-        else:
+        elif (sk_count >= skip_lines_num): # This only allows the data to be pulled in if
+            # Current Line's data
             cData = []
 
+            # Going through each of the comma seperated values to fill the current data matrix
             for i in range(len(spltLine)):
-                # print(np.array(float(spltLine[i].strip())))
-                # print(spltLine[i])
                 if (spltLine[i] != ""):
                     cData.append(np.array(float(spltLine[i].strip())))
+
+            # If the data is a matrix, it is handled differently
             if (allData.type[-1] == "matrix"):
+                # If this is the first time the matrix has been touched
+                #   (first time step pulled in) the the based numpy array is created
+                # Else the numpy array is built
                 if (count_line == 2):
                     allData.data.append(np.array(cData))
                 else:
                     allData.data[-1] = np.vstack((allData.data[-1],cData))
 
+            # Else the data is handled as a single value
+            else:
+                # If this is the first time the matrix has been touched
+                #   (first time step pulled in) the the based numpy array is created
+                # Else the numpy array is built
+                if (count_line == 2):
+                    for i in range(len(cData)):
+                        allData.data.append(np.array(cData[i]))
+
+                else:
+                    i2 = len(cData)
+                    for i in range(len(cData)):
+                        allData.data[i-i2] = np.vstack((allData.data[i-i2],cData[i]))
+
+            # Resetting the skip counter
+            sk_count = 1
+
+        else:
+            sk_count = sk_count + 1
+
         count_line = count_line + 1
-        if (count_line > 100):
-            break
-
-    break
+        # if (count_line > 1e10):
+        #     break
 
 
-# print(allData.data[0][:][:].shape)
-print(allData.data[0][:][:].shape)
+# %% Plotting
+
+# %% Finding the desired names
+print(allData.name)
+
+# %%
+time = find_data(allData.data,allData.name,"el_time")
+
+
+desired_data = find_data(allData.data,allData.name,myStr)
+plt.plot(time, desired_data)
+plt.title(myStr)
+plt.show()
